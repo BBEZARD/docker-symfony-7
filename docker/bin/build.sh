4e90795d04fd12868f3187d3c185612d4b96b99c
BBEZARD <contact@hourraweb.fr>
2024-07-07 10:02:25 +0200

    Paramétrage de nginx et Docker

--- docker/bin/build.sh
+++ docker/bin/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Mets à jour les conteneurs Docker du projet
+docker compose --env-file "docker/config/.env" -f docker/docker-compose.yaml build
 docker compose --env-file "docker/config/.env" -f docker/docker-compose.yaml up -d
